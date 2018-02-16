422af6fc57842e0999593af3f966607a27e6c055
codylewis <codylewis87@gmail.com>
2018-02-16 12:15:59 -0600

    add meetup logo & link to front page

--- front-page.php
+++ front-page.php
@@ -10,12 +10,17 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
              alt="Milwaukee Hacker Hours logo" class="img-fluid rounded ml-auto mr-auto d-block mb-5">
         <h1 class="text-center display-4 display-5 mb-5"><?php echo get_the_content(); ?></h1>
         <div class="row">
-            <div class="ml-auto col-sm-6 col-lg-3 text-center text-sm-right">
+            <div class="ml-auto col-sm-4 col-lg-3 text-center text-sm-right">
                 <a href="<?php the_field('twitter_widget_url'); ?>" class="twitter-follow-button"
                    data-show-count="false"
                    data-size="large">Follow @MKEHackerHours</a>
             </div>
-            <div class="mr-auto col-sm-6 col-lg-3 text-center text-sm-left mt-3 mt-sm-0">
+            <div class="col-sm-4 col-lg-3 text-center mt-3 mt-sm-0">
+                <a href="<?php the_field('meetup_url'); ?>" class="d-inline-flex">
+                    <img src="<?php echo get_template_directory_uri() . '/img/meetup_logo.png'; ?>" alt="">
+                </a>
+            </div>
+            <div class="mr-auto col-sm-4 col-lg-3 text-center mt-3 mt-sm-0 text-sm-left">
                 <a href="<?php the_field('slack_invite_link'); ?>" class="d-inline-flex">
                     <img src="<?php echo get_template_directory_uri() . '/img/slack_rgb.png'; ?>" alt="">
                 </a>
